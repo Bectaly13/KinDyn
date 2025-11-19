d93b46c4c9c5dc1c724c574fec81dc815cee9777
Bectaly13 <dodocad13@gmail.com>
2025-11-19 03:11:19 +0100

    basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,36 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'welcome',
+    loadComponent: () => import('./pages/welcome/welcome.page').then( m => m.WelcomePage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'planning',
+    loadComponent: () => import('./pages/planning/planning.page').then( m => m.PlanningPage)
+  },
+  {
+    path: 'session',
+    loadComponent: () => import('./pages/session/session.page').then( m => m.SessionPage)
+  },
+  {
+    path: 'followup',
+    loadComponent: () => import('./pages/followup/followup.page').then( m => m.FollowupPage)
   },
 ];
