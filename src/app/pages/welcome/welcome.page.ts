3d5b2008edd293d9d99ffb611447a54184a26886
Bectaly13 <dodocad13@gmail.com>
2025-11-21 00:47:30 +0100

    welcome

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, ViewWillEnter } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+
+import { StorageService } from 'src/app/services/storage';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, CommonModule, FormsModule]
 })
-export class WelcomePage implements OnInit {
+export class WelcomePage implements ViewWillEnter {
+  loadingMessage: string = "";
 
-  constructor() { }
+  constructor(
+    private storage: StorageService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  async ionViewWillEnter() {
+    this.loadingMessage = "...";
+
+    let lang_data = await this.storage.get("lang");
+    if(!lang_data) {
+      this.storage.set("lang", "FR");
+    }
+
+    if(lang_data == "FR") {
+      this.loadingMessage = "Préférences de langue récupérées.";
+    }
+    else {
+      this.loadingMessage = "Language preferences fetched.";
+    }
+
+    if(lang_data == "FR") {
+      this.loadingMessage = "Redirection...";
+    }
+    else {
+      this.loadingMessage = "Redirecting...";
+    }
+
+    setTimeout(() => {
+        this.router.navigate(["login"]);
+    }, 4000);    
   }
-
 }
