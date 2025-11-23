bbca58144b3474f73625f30485c5e047e5e103ac
Bectaly13 <dodocad13@gmail.com>
2025-11-23 02:52:03 +0100

    build apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'frontend',
+  appId: 'io.ionic.kindyn',
+  appName: 'KinDyn',
   webDir: 'www'
 };
 
